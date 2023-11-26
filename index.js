6d44e55725dfbd1a0a582fff1aed575820c96803
Bipon Roy <biponroy400@gmail.com>
2023-11-27 00:33:43 +0600

    Added middleware and users and services api

--- index.js
+++ index.js
@@ -23,12 +23,46 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         //   await client.connect();
         const userCollection = client.db("employeeDB").collection("user");
+        const serviceCollection = client.db("employeeDB").collection("services");
+        const opinionCollection = client.db("employeeDB").collection("opinion");
+        const worksheetCollection = client.db("employeeDB").collection("worksheet");
         //jwt related api
-
         app.post("/jwt", async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.Access_Token_Secret, { expiresIn: "1h" });
             res.send({ token });
+        });
+        // middlewares
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "unauthorized access" });
+            }
+            const token = req.headers.authorization.split(" ")[1];
+            jwt.verify(token, process.env.Access_Token_Secret, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "unauthorized access" });
+                }
+                req.decoded = decoded;
+                next();
+            });
+        };
+        const verifyEmployee = async (req, res, next) => {
+            const email = req.decoded.email;
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isEmployee = user?.role.toLowerCase() === "employee";
+            console.log(isEmployee);
+            if (!isEmployee) {
+                return res.status(403).send({ message: "forbidden access" });
+            }
+            next();
+        };
+
+        //service related api
+        app.get("/services", async (req, res) => {
+            const result = await serviceCollection.find().toArray();
+            res.send(result);
         });
 
         //user related api
@@ -40,6 +74,25 @@ async function run() {
                 return res.send({ message: "User Already Exist", insertedId: null });
             }
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        //opinion related api
+        app.post("/opinion", async (req, res) => {
+            const message = req.body;
+            const result = await opinionCollection.insertOne(message);
+            res.send(result);
+        });
+
+        //employee related api
+        app.get("/worksheet", verifyToken, verifyEmployee, async (req, res) => {
+            const result = await worksheetCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post("/worksheet", verifyToken, verifyEmployee, async (req, res) => {
+            const data = req.body;
+            const result = await worksheetCollection.insertOne(data);
             res.send(result);
         });
         // Send a ping to confirm a successful connection
